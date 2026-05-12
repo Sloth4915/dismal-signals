9c0440b61c98cf8adf2241c29831a184f19e3037
Sloth4915 <148401810+Sloth4915@users.noreply.github.com>
2026-05-12 09:44:56 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,267 @@
+"use strict";
+
+// BASE
+
+const debug = true
+
+//#region Initial Setup
+console.clear()
+
+document.body.style["margin"] = "0"
+document.body.style["display"] = "flex"
+document.body.style["justify-content"] = "center"
+document.body.style["align-items"] = "center"
+document.body.style["width"] = "100vw"
+document.body.style["height"] = "100vh"
+document.body.style["background-color"] = "black"
+
+let width = 1200
+let height = 800
+let canvas = document.createElement("canvas")
+document.body.appendChild(canvas)
+canvas.width = width
+canvas.height = height
+canvas.style.maxWidth = "100vw"
+canvas.style.maxHeight = "100vh"
+let draw = canvas.getContext("2d")
+draw.fillStyle = "purple"
+draw.fillRect(0,0,width,height)
+//#endregion Initial Setup
+
+//#region Engine
+const CollisionLayers = Object.freeze({
+    LIGAND: 0,
+    RECEPTOR: 1,
+})
+
+let dt = 0
+
+class Entity {
+    constructor() {
+        this.x = 100
+        this.y = 100
+
+        this.collisions = false
+        this.physicsWidth = 100
+        this.physicsHeight = 100
+        this.personalLayers = []
+        this.collisionLayers = []
+        this.vx = 0
+        this.vy = 0
+
+        this.hasMouseCollision = false
+        this.mouseWidth = 100
+        this.mouseHeight = 100
+        this.mousePriority = 0
+        this.hovered = false
+        this.mouseDown = false
+    }
+    _tick() {
+        this._draw()
+        this.x += this.vx * dt
+        this.y += this.vy * dt
+
+        if (this.hasMouseCollision) {
+            this.hovered = (this.x < mx && mx < this.x + this.mouseWidth) && (this.y < my && my < this.y + this.mouseHeight)
+            if (this.hovered && !_hoveredEntities.includes(this)) _hoveredEntities.push(this)
+            else if (!this.hovered && _hoveredEntities.includes(this)) _hoveredEntities.splice(_hoveredEntities.indexOf(this), 1)
+            this.mouseDown = this === downEntity
+        }
+
+        if (debug) {
+            this._debugDraw()
+        }
+    }
+    _draw() {
+
+    }
+    _debugDraw() {
+        if (this.collisions) {
+            draw.lineWidth = "1"
+            draw.strokeStyle = draw.fillStyle = "yellow"
+            draw.font = "16px Arial"
+            draw.strokeRect(this.x, this.y, this.physicsWidth, this.physicsHeight)
+            draw.fillText(`Layers${this.personalLayers}, ${this.collisionLayers}`, this.x, this.y)
+
+            draw.beginPath();
+            draw.moveTo(this.x + this.physicsWidth / 2, this.y + this.physicsHeight / 2);
+            draw.lineTo(this.x + this.physicsWidth / 2 + this.vx, this.y + this.physicsHeight / 2 + this.vy);
+            draw.stroke();
+
+            if (this.hasMouseCollision) {
+                if (this.hovered) {
+                    draw.lineWidth = "4"
+                    draw.strokeStyle = draw.fillStyle = "green"
+                    draw.strokeRect(this.x + 4, this.y + 4, this.physicsWidth - 8, this.physicsHeight - 8)
+                }
+                if (this.mouseDown) {
+                    draw.strokeStyle = draw.fillStyle = "blue"
+                    draw.strokeRect(this.x + 8, this.y + 8, this.physicsWidth - 16, this.physicsHeight - 16)
+                }
+            }
+        }
+    }
+}
+
+/* Holds multiple entities together as one */
+class Group extends Entity {
+    constructor() {
+        super()
+        this.entities = []
+        this.mode = Group.MovementMode.CARTESIAN
+    }
+    _tick() {
+        super._tick()
+        for (let e of this.entities) {
+            e.x += this.vx * dt
+            e.y += this.vy * dt
+            e._tick()
+        }
+    }
+    addChild(e) {
+        this.entities.push(e)
+    }
+
+    static MovementMode = {
+        CARTESIAN: 0,
+        POLAR: 1,
+    }
+}
+
+class Sprite extends Entity {
+    constructor() {
+        super()
+        this.r = 0
+        this.vr = 0
+        this.scaleX = 1
+        this.scaleY = 1
+
+        this.drawCanvas = new OffscreenCanvas(this.physicsWidth, this.physicsHeight)
+        this.drawCtx = this.drawCanvas.getContext("2d")
+
+        /** rect, ellipse, triangle, custom, img */
+        this.shape = "rect"
+    }
+    _tick() {
+        super._tick()
+        this.r += this.vr * dt
+    }
+    _draw() {
+        super._draw()
+        draw.save()
+        draw.translate(this.x + this.physicsWidth / 2, this.y + this.physicsHeight / 2);
+        draw.rotate(this.r)
+        draw.drawImage(
+            this.drawCanvas,
+            -this.physicsWidth / 2 * this.scaleX,
+            -this.physicsHeight / 2 * this.scaleY,
+            this.scaleX * this.physicsWidth,
+            this.scaleY * this.physicsHeight
+        )
+        draw.restore()
+    }
+    _drawExtra() {
+        return null
+    }
+
+    static Shape = {
+        RECT: 0,
+        ELLIPSE: 1,
+        CUSTOM: 2,
+        IMAGE: 3,
+    }
+
+    setImage(src) {
+        let img = new Image()
+        img.src = src
+        img.addEventListener("load", () => {
+            this.drawCtx.drawImage(img, 0, 0, 100, 100)
+        })
+    }
+}
+
+class UIElement extends Sprite {
+    constructor() {
+        super();
+        this.hasMouseCollision = true
+    }
+}
+
+class Particle extends Group {
+    constructor() {
+        super();
+
+    }
+}
+
+const World = new Group()
+World.color = "purple"
+let _time = null
+let timeElapsed = 0
+
+let mx = 0
+let my = 0
+let mdown = false
+let _hoveredEntities = []
+let downEntity = null
+
+canvas.addEventListener("mousemove", (e) => {
+    mx = ((e.clientX - canvas.offsetLeft) / canvas.offsetWidth) * canvas.width
+    my = ((e.clientY - canvas.offsetTop) / canvas.offsetHeight) * canvas.height
+})
+document.addEventListener("mousedown", (e) => {
+    mdown = true
+})
+document.addEventListener("mouseup", (e) => {
+    mdown = false
+})
+document.addEventListener("mouseleave", (e) => {
+    mdown = false
+})
+
+
+function _tick(a) {
+    if (_time === null) {
+        _time = a
+        requestAnimationFrame(_tick)
+        return
+    }
+    dt = (a - _time)/1000
+
+    draw.fillStyle = World.color
+    draw.fillRect(0,0,width,height)
+    World._tick()
+
+    testEntity.vx = 80 * Math.sin(timeElapsed * 0.5)
+    testEntity.vy = 80 * Math.cos(timeElapsed * 0.5)
+
+    if (debug) {
+        draw.font = "16px Arial"
+        draw.fillStyle = "yellow"
+        draw.fillText(`${Math.round(1/dt)}fps`,0,16)
+    }
+
+    draw.fillStyle = "yellow"
+    draw.fillRect(mx,my,4,4)
+
+    downEntity = {mousePriority: Infinity}
+    if (mdown) {
+        for (let entity of _hoveredEntities) {
+            if (entity.mousePriority < downEntity.mousePriority) downEntity = entity
+        }
+    }
+
+    _time = a
+    timeElapsed += dt
+    requestAnimationFrame(_tick)
+}
+requestAnimationFrame(_tick)
+
+//#endregion Engine
+
+let testEntity = new Sprite()
+testEntity.setImage("ligandtest.png")
+testEntity.collisions = true
+testEntity.hasMouseCollision = true
+console.log(testEntity)
+World.addChild(testEntity)
